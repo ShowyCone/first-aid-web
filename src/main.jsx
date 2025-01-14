bd197996b3197a6e7e77b9b15ea292b3e1e6b80b
ShowyCone <alexrafael2003@gmail.com>
2025-01-14 15:00:55 -0400

    delete import App line 😐

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import Introduction from './components/Introduction'
 import Menu from './components/Menu'
 import Learn from './components/Learn'
