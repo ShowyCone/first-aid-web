506370e40ce165b2581c31b38e62b6732a853cfb
ShowyCone <alexrafael2003@gmail.com>
2025-01-14 16:54:25 -0400

    update index.html DOCTYPE and script source, remove redirects file, adjust Vite base URL

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://showycone.github.io/first-aid-web/',
+  base: '/first-aid-web/',
 })
