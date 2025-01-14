82d301b523c8134151ec873cb3d5998577a9a755
ShowyCone <alexrafael2003@gmail.com>
2025-01-14 16:01:55 -0400

    Revert "delete import App line 😐"
    This reverts commit bd197996b3197a6e7e77b9b15ea292b3e1e6b80b.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App.jsx'
 import Introduction from './components/Introduction'
 import Menu from './components/Menu'
 import Learn from './components/Learn'
