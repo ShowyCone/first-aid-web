1aa42b576b65489209112ad5ab1b24a291d0ff19
ShowyCone <alexrafael2003@gmail.com>
2025-01-14 17:08:02 -0400

    refactor: restructure routing by introducing App component and remove GitHub Actions deployment workflow

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Introduction from './components/Introduction'
+import Menu from './components/Menu'
+import Learn from './components/Learn'
+import QuickConsult from './components/QuickConsult'
+import Class from './components/Class'
+
+function App() {
+  return (
+    <Router basename='/first-aid-web'>
+      <Routes>
+        <Route path='/' element={<Introduction />} />
+        <Route path='/menu' element={<Menu />} />
+        <Route path='/learn' element={<Learn />} />
+        <Route path='/quick-consult' element={<QuickConsult />} />
+        <Route path='/class/:id' element={<Class />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,38 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Introduction from './components/Introduction'
-import Menu from './components/Menu'
-import Learn from './components/Learn'
-import QuickConsult from './components/QuickConsult'
-import Class from './components/Class'
-import { createHashRouter, RouterProvider } from 'react-router-dom'
-
-const router = createHashRouter([
-  {
-    path: '/',
-    element: <Introduction />,
-  },
-  {
-    path: '/menu',
-    element: <Menu />,
-  },
-  {
-    path: '/learn',
-    element: <Learn />,
-  },
-  {
-    path: '/quick-consult',
-    element: <QuickConsult />,
-  },
-  {
-    path: '/class/:id',
-    element: <Class />,
-  },
-])
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>
 )
